328dc9d823fb508d759cc5d8ec92257737e595d2
Rick Volosky <rivolosky@springthrough.com>
2017-09-22 15:42:12 -0400

    Begin implementation of User Accounts management

---
+++ RickFreelanceWebApp/RickFreelanceWebApp/Controllers/UserManagementController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RickFreelanceWebApp.Models;
+using RickFreelanceWebApp.Models.UserManagementViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RickFreelanceWebApp.Controllers
+{
+    [Authorize]
+    public class UserManagementController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserManagementController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var model = new IndexViewModel
+            {
+                Users = _userManager.Users
+            };
+            return View(model);
+        }
+    }
+}
